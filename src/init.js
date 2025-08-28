195ae815477bea37d9a5dbc5f42daebec5bd16db
Andrew Gubsky <andrei.gubsky@gmail.com>
2025-08-28 14:41:50 +0300

    Fix timeouts

--- src/init.js
+++ src/init.js
@@ -43,206 +43,211 @@ const css = './css/';
 
 
 /* Deleting all tamplate content from dirs /partials and /css */
-// fs.readdir(partials, (err, files) => {
-//     if (err) {
-//       console.error('Ошибка чтения папки:', err);
-//       return;
-//     }
 
-//     files.forEach(file => {
-//       const filePath = path.join(partials, file);
-//       // Проверяем, что это файл, а не подпапка (если нужно удалять только файлы)
-//       fs.stat(filePath, (statErr, stats) => {
-//         if (statErr) {
-//           console.error(`Ошибка получения статистики файла ${filePath}:`, statErr);
-//           return;
-//         }
-//         if (stats.isFile()) {
-//           fs.rm(filePath, (rmErr) => {
-//             if (rmErr) {
-//               console.error(`Ошибка при удалении файла ${filePath}:`, rmErr);
-//             } else {
-//               console.log(`Файл ${filePath} успешно удален.`);
-//             }
-//           });
-//         }
-//       });
-//     });
-//   });
+function deleteDefaultFiles() {
+  fs.readdir(partials, (err, files) => {
+    if (err) {
+      console.error('Ошибка чтения папки:', err);
+      return;
+    }
 
-//   fs.readdir(css, (err, files) => {
-//     if (err) {
-//       console.error('Ошибка чтения папки:', err);
-//       return;
-//     }
+    files.forEach(file => {
+      const filePath = path.join(partials, file);
+      // Проверяем, что это файл, а не подпапка (если нужно удалять только файлы)
+      fs.stat(filePath, (statErr, stats) => {
+        if (statErr) {
+          console.error(`Ошибка получения статистики файла ${filePath}:`, statErr);
+          return;
+        }
+        if (stats.isFile()) {
+          fs.rm(filePath, (rmErr) => {
+            if (rmErr) {
+              console.error(`Ошибка при удалении файла ${filePath}:`, rmErr);
+            } else {
+              console.log(`Файл ${filePath} успешно удален.`);
+            }
+          });
+        }
+      });
+    });
+  });
 
-//     files.forEach(file => {
-//       const filePath = path.join(css, file);
-//       // Проверяем, что это файл, а не подпапка (если нужно удалять только файлы)
-//       fs.stat(filePath, (statErr, stats) => {
-//         if (statErr) {
-//           console.error(`Ошибка получения статистики файла ${filePath}:`, statErr);
-//           return;
-//         }
-//         if (stats.isFile()) {
-//           fs.rm(filePath, (rmErr) => {
-//             if (rmErr) {
-//               console.error(`Ошибка при удалении файла ${filePath}:`, rmErr);
-//             } else {
-//               console.log(`Файл ${filePath} успешно удален.`);
-//             }
-//           });
-//         }
-//       });
-//     });
-//   });
+  fs.readdir(css, (err, files) => {
+    if (err) {
+      console.error('Ошибка чтения папки:', err);
+      return;
+    }
 
-// sectionName.forEach((section)=> {
-//     if(section==="header"){
-//         fs.writeFile(path.join(partials, `${section}.html`), 
-//     `<header class="section"><div class="container"></div></header>`, (err) => {
-//         if (err) {
-//         console.error('Error:', err);
-//         return;
-//         }
-//         console.log('File created successfuly!');
-//     });
-//     }
+    files.forEach(file => {
+      const filePath = path.join(css, file);
+      // Проверяем, что это файл, а не подпапка (если нужно удалять только файлы)
+      fs.stat(filePath, (statErr, stats) => {
+        if (statErr) {
+          console.error(`Ошибка получения статистики файла ${filePath}:`, statErr);
+          return;
+        }
+        if (stats.isFile()) {
+          fs.rm(filePath, (rmErr) => {
+            if (rmErr) {
+              console.error(`Ошибка при удалении файла ${filePath}:`, rmErr);
+            } else {
+              console.log(`Файл ${filePath} успешно удален.`);
+            }
+          });
+        }
+      });
+    });
+  });
+}
 
-//     if(section==="footer"){
-//         fs.writeFile(path.join(partials, `${section}.html`), 
-//     `<footer class="section"><div class="container"></div></footer>`, (err) => {
-//         if (err) {
-//         console.error('Error:', err);
-//         return;
-//         }
-//         console.log('File created successfuly!');
-//     });
-//     }
-//     fs.writeFile(path.join(partials, `${section}.html`), 
-//     `<section class="section"><div class="container"></div></section>`, (err) => {
-//         if (err) {
-//         console.error('Error:', err);
-//         return;
-//         }
-//         console.log('File created successfuly!');
-//     });
-// });
-
-// sectionName.forEach((section)=> {
-//     fs.writeFile(path.join(css, `${section}.css`), 
-//     `/*Styles for section ${section}*/ `, (err) => {
-//         if (err) {
-//         console.error('Error:', err);
-//         return;
-//         }
-//         console.log('File created successfuly!');
-//     });
-// });
-// fs.writeFile(path.join(css, `reset.css`), 
-//     `/*Reset styles*/
-// h1,
-// h2,
-// h3,
-// h4,
-// h5,
-// h6,
-// p {
-//   margin: 0;
-// }
-
-// p:last-child {
-//   margin-bottom: 0;
-// }
-
-// ul {
-//   margin: 0;
-//   padding: 0;
-//   list-style: none;
-// }
-
-// a {
-//   color: currentColor;
-//   text-decoration: none;
-// }
-
-// button {
-//   cursor: pointer;
-// }
-
-// img {
-//   display: block;
-//   max-width: 100%;
-//   height: auto;
-// }`, (err) => {
-//         if (err) {
-//         console.error('Error:', err);
-//         return;
-//         }
-//         console.log('File created successfuly!');
-// });
-// fs.writeFile(path.join(css, `base.css`), 
-//     `/*Base styles*/ 
-// :root {}
-// body {}`, (err) => {
-//         if (err) {
-//         console.error('Error:', err);
-//         return;
-//         }
-//         console.log('File created successfuly!');
-// });
-// fs.writeFile(path.join(css, `container.css`), 
-//     `/* Styles for Mobile devices */
-//     .container {
-//       min-width: 320px;
-//       max-width: 375px;
-//       margin: 0 auto;
-//       padding: 0 16px;
-//     }
-//     /* Styles for Tablet devices */
-//     @media only screen and (min-width: 768px) {
-//       .container {
-//         max-width: 768px;
-//       }
-//     }
-//     /* Styles for Desktop devices */
-//     @media only screen and (min-width: 1200px) {
-//       .container {
-//         max-width: 1200px;
-//       }
-//     }`, (err) => {
-//         if (err) {
-//         console.error('Error:', err);
-//         return;
-//         }
-//         console.log('File created successfuly!');
-// });
-
-
-/* Creating style.css*/
-fs.readdir(css, (err, files) => {
-  if (err) {
-    console.error('Ошибка при чтении директории:', err); // Обработка ошибки
-    return;
-  }
-  files.forEach(file => {
-    if (file !== 'styles.css' || file !== 'style.css')
-      fs.appendFile(path.join(css, `styles.css`),
-        `@import url('./${file}');\n`, (err) => {
+function createPartialsFiles() {
+  sectionName.forEach((section) => {
+    if (section === "header") {
+      fs.writeFile(path.join(partials, `${section}.html`),
+        `<header class="section"><div class="container"></div></header>`, (err) => {
           if (err) {
             console.error('Error:', err);
             return;
           }
-          console.log(`File ${file} imported successfuly!`);
+          console.log('File created successfuly!');
         });
+    }
+
+    if (section === "footer") {
+      fs.writeFile(path.join(partials, `${section}.html`),
+        `<footer class="section"><div class="container"></div></footer>`, (err) => {
+          if (err) {
+            console.error('Error:', err);
+            return;
+          }
+          console.log('File created successfuly!');
+        });
+    }
+    fs.writeFile(path.join(partials, `${section}.html`),
+      `<section class="section"><div class="container"></div></section>`, (err) => {
+        if (err) {
+          console.error('Error:', err);
+          return;
+        }
+        console.log('File created successfuly!');
+      });
   });
-});
 
+  sectionName.forEach((section) => {
+    fs.writeFile(path.join(css, `${section}.css`),
+      `/*Styles for section ${section}*/ `, (err) => {
+        if (err) {
+          console.error('Error:', err);
+          return;
+        }
+        console.log('File created successfuly!');
+      });
+  });
+  fs.writeFile(path.join(css, `reset.css`),
+    `/*Reset styles*/
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p {
+    margin: 0;
+  }
+  
+  p:last-child {
+    margin-bottom: 0;
+  }
+  
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+  
+  a {
+    color: currentColor;
+    text-decoration: none;
+  }
+  
+  button {
+    cursor: pointer;
+  }
+  
+  img {
+    display: block;
+    max-width: 100%;
+    height: auto;
+  }`, (err) => {
+    if (err) {
+      console.error('Error:', err);
+      return;
+    }
+    console.log('File created successfuly!');
+  });
+  fs.writeFile(path.join(css, `base.css`),
+    `/*Base styles*/ 
+  :root {}
+  body {}`, (err) => {
+    if (err) {
+      console.error('Error:', err);
+      return;
+    }
+    console.log('File created successfuly!');
+  });
+  fs.writeFile(path.join(css, `container.css`),
+    `/* Styles for Mobile devices */
+      .container {
+        min-width: 320px;
+        max-width: 375px;
+        margin: 0 auto;
+        padding: 0 16px;
+      }
+      /* Styles for Tablet devices */
+      @media only screen and (min-width: 768px) {
+        .container {
+          max-width: 768px;
+        }
+      }
+      /* Styles for Desktop devices */
+      @media only screen and (min-width: 1200px) {
+        .container {
+          max-width: 1200px;
+        }
+      }`, (err) => {
+    if (err) {
+      console.error('Error:', err);
+      return;
+    }
+    console.log('File created successfuly!');
+  });
+}
 
-/* Createng index.html */
+/* Import into style.css*/
+function importStylePartials() {
+  fs.readdir(css, (err, files) => {
+    if (err) {
+      console.error('Ошибка при чтении директории:', err); // Обработка ошибки
+      return;
+    }
+    files.forEach(file => {
+      if (file !== 'styles.css' || file !== 'style.css')
+        fs.appendFile(path.join(css, `styles.css`),
+          `@import url('./${file}');\n`, (err) => {
+            if (err) {
+              console.error('Error:', err);
+              return;
+            }
+            console.log(`File ${file} imported successfuly!`);
+          });
+    });
+  });
+}
 
-async function appendToFileStart() {
-  await fs.appendFile(`index.html`, `
+/* Load into index.html */
+
+function loadHeaderIntoHtml() {
+  fs.writeFile(`index.html`, `
   <!DOCTYPE html>
             <html lang="en">
               <head>
@@ -261,32 +266,31 @@ async function appendToFileStart() {
       console.log(`Head imported successfuly!`);
     });
 }
-appendToFileStart();
+function loadPartialsIntoHtml() {
+  fs.readdir(partials, (err, files) => {
+    if (err) {
+      console.error('Ошибка при чтении директории:', err); // Обработка ошибки
+      return;
+    }
 
-fs.readdir(partials, (err, files) => {
-  if (err) {
-    console.error('Ошибка при чтении директории:', err); // Обработка ошибки
-    return;
-  }
+    files.forEach(file => {
 
-  files.forEach(file => {
-
-    fs.appendFile(
-      `index.html`,
-      `<load src="./partials/${file}" />\n`,
-      (err) => {
-        if (err) {
-          console.error('Error:', err);
-          return;
-        }
-        console.log(`File ${file} load successfuly!`);
-      });
-    //console.log(file); // Выводим имя каждого файла
+      fs.appendFile(
+        `index.html`,
+        `<load src="./partials/${file}" />\n`,
+        (err) => {
+          if (err) {
+            console.error('Error:', err);
+            return;
+          }
+          console.log(`File ${file} load successfuly!`);
+        });
+      //console.log(file); // Выводим имя каждого файла
+    });
   });
-});
-
-async function appendToFileEnd() {
-  await fs.appendFile(`index.html`, `
+}
+function loadFooterIntoHtml() {
+  fs.appendFile(`index.html`, `
   <script type="module" src="./main.js"></script>
   </body>
 </html>`,
@@ -299,10 +303,19 @@ async function appendToFileEnd() {
     });
 }
 
+deleteDefaultFiles();
+
 setTimeout(() => {
-  appendToFileEnd();
+  createPartialsFiles();
+  importStylePartials();
+  loadHeaderIntoHtml()
 }, 1000);
 
-
+setTimeout(() => {
+  loadPartialsIntoHtml()
+}, 2000);
+setTimeout(() => {
+  loadFooterIntoHtml()
+}, 3000);
 
 console.log("Thanks! I'm alive!");
